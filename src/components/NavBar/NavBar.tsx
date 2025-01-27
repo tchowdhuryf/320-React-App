5602b72e3303b6787558e67e126527461dfe2faa
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-01-26 21:16:04 -0500

    feat(nav-bar): add navmenu property

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,11 +5,15 @@ interface NavBarProps {
   logo?: ReactNode;
   cartButton?: ReactNode;
   searchBar?: ReactNode;
+  navMenu?: ReactNode;
 }
 
-const NavBar: React.FC<NavBarProps> = ({ logo, cartButton, searchBar }) => {
+const NavBar: React.FC<NavBarProps> = ({ logo, cartButton, searchBar, navMenu }) => {
   return (
     <nav className="navbar">
+      <div className="navbar__left">
+        {navMenu}
+      </div>
       <div className="navbar__logo">{logo || <span>Logo</span>}</div>
       <div className="navbar__right">
         {searchBar && <div className="navbar__search-bar">{searchBar}</div>}
