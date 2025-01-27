57906dde0703b5673c034b68f9430505ab730ddc
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2025-01-26 19:25:53 -0500

    feat(nav-bar): add cartbutton property

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,12 +3,17 @@ import "./NavBar.css";
 
 interface NavBarProps {
   logo?: ReactNode;
+  cartButton?: ReactNode;
+
 }
 
-const NavBar: React.FC<NavBarProps> = ({ logo }) => {
+const NavBar: React.FC<NavBarProps> = ({ logo, cartButton }) => {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">{logo}</div>
+      <div className="navbar__logo">{logo || <span>Logo</span>}</div>
+      <div className="navbar__right">
+        {cartButton && <div className="navbar__cart-button">{cartButton}</div>}
+      </div>
     </nav>
   );
 };
